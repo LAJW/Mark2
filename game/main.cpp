a50ed36305fa22d949df9ecdd27b300c3dc6c58e
Lukasz Wrona <lukasz.andrzej.wrona@gmail.com>
2017-06-04 19:30:13 +0200

    FIX: NVIDIA Optimus - use dedicated GPU.

--- game/main.cpp
+++ game/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include "app.h"
 
+extern "C" {
+	// Enable dedicated graphics for NVIDIA
+	__declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+	// Enable dedicated graphics for AMD Radeon
+	__declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+
 int main(const int argc, const char* argv[]) {
 	try {
 		mark::app app(argc, argv);
