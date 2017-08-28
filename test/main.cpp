29a4b03a77a9788e74de598d50b118121001bef9
Lukasz Wrona <lukasz.andrzej.wrona@gmail.com>
2017-08-28 19:28:11 +0100

    TEST: Upgrade unit test to the optinoal collide version.

--- test/main.cpp
+++ test/main.cpp
@@ -8,10 +8,12 @@
 TEST_CASE("should return nullptr or possibly crash") {
 	mark::resource::manager_stub rm;
 	const auto map = mark::map::make_square(rm);
-	const auto result = map.collide({
+	const auto maybe_result = map.collide({
 		{ 0, 0 },
 		{ 1000, 0 }
 	});
+	REQUIRE(maybe_result.has_value());
+	const auto result = maybe_result.value();
 	REQUIRE(result.x == Approx(8.5 * mark::map::tile_size));
 	REQUIRE(result.y == Approx(0));
 }
