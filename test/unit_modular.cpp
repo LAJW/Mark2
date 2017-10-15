66d71d6f20c49924d086383df528f25462e75f11
Lukasz Wrona <lukasz.andrzej.wrona@gmail.com>
2017-10-15 20:49:43 +0100

    TEST: Treat errors as warnings in tests.

--- test/unit_modular.cpp
+++ test/unit_modular.cpp
@@ -22,7 +22,7 @@ TEST_CASE("Create modular with a core") {
 	std::unique_ptr<mark::module::base> core =
 		std::make_unique<mark::module::core>(rm);
 	const auto& core_ref = *core;
-	modular.attach({ -1, -1 }, core);
+	REQUIRE(modular.attach({ -1, -1 }, core) == mark::error::code::success);
 	REQUIRE(core == nullptr);
 	REQUIRE(core_ref.pos() == mark::vector<double>(1, 2));
 	REQUIRE(core_ref.grid_pos() == mark::vector<int>(-1, -1));
@@ -41,7 +41,7 @@ TEST_CASE("Try creating modular with two cores") {
 		std::make_unique<mark::module::core>(rm);
 	std::unique_ptr<mark::module::base> core2 =
 		std::make_unique<mark::module::core>(rm);
-	modular.attach({ -1, -1 }, core1);
+	REQUIRE(mark::error::code::success == modular.attach({ -1, -1 }, core1));
 	REQUIRE(mark::error::code::success != modular.attach({ 1, -1 }, core2));
 }
 
@@ -51,7 +51,7 @@ TEST_CASE("Create modular with a turret to the right") {
 	mark::unit::modular modular(world, { 0, 0 }, 0);
 	std::unique_ptr<mark::module::base> core =
 		std::make_unique<mark::module::core>(rm);
-	modular.attach({ -1, -1 }, core);
+	REQUIRE(mark::error::code::success == modular.attach({ -1, -1 }, core));
 
 	mark::module::turret::info info;
 	info.resource_manager = &rm;
@@ -59,7 +59,7 @@ TEST_CASE("Create modular with a turret to the right") {
 		std::make_unique<mark::module::turret>(info);
 	const auto& turret_ref = *turret;
 
-	modular.attach({ 1, -1 }, turret);
+	REQUIRE(mark::error::code::success == modular.attach({ 1, -1 }, turret));
 	REQUIRE(turret_ref.grid_pos() == mark::vector<int>(1, -1));
 	REQUIRE(turret_ref.pos().x == Approx(mark::module::size * 2));
 	REQUIRE(turret_ref.pos().y == Approx(0));
@@ -71,7 +71,7 @@ TEST_CASE("Attach turret in all possible positions") {
 	mark::unit::modular modular(world, { 0, 0 }, 0);
 	std::unique_ptr<mark::module::base> core =
 		std::make_unique<mark::module::core>(rm);
-	modular.attach({ -1, -1 }, core);
+	REQUIRE(mark::error::code::success == modular.attach({ -1, -1 }, core));
 
 	mark::module::turret::info info;
 	info.resource_manager = &rm;
@@ -80,43 +80,43 @@ TEST_CASE("Attach turret in all possible positions") {
 	const auto& turret_ref = *turret;
 
 	// Right
-	modular.attach({ 1, -1 }, turret);
+	REQUIRE(mark::error::code::success == modular.attach({ 1, -1 }, turret));
 	turret = modular.detach({ 1, -1 });
 
-	modular.attach({ 1, -2 }, turret);
+	REQUIRE(mark::error::code::success == modular.attach({ 1, -2 }, turret));
 	turret = modular.detach({ 1, -2 });
 
-	modular.attach({ 1, 0 }, turret);
+	REQUIRE(mark::error::code::success == modular.attach({ 1, 0 }, turret));
 	turret = modular.detach({ 1, 0 });
 
 	// Left
-	modular.attach({ -3, -1 }, turret);
+	REQUIRE(mark::error::code::success == modular.attach({ -3, -1 }, turret));
 	turret = modular.detach({ -3, -1 });
 
-	modular.attach({ -3, -2 }, turret);
+	REQUIRE(mark::error::code::success == modular.attach({ -3, -2 }, turret));
 	turret = modular.detach({ -3, -2 });
 
-	modular.attach({ -3, 0 }, turret);
+	REQUIRE(mark::error::code::success == modular.attach({ -3, 0 }, turret));
 	turret = modular.detach({ -3, 0 });
 
 	// Top
-	modular.attach({ -1, 1 }, turret);
+	REQUIRE(mark::error::code::success == modular.attach({ -1, 1 }, turret));
 	turret = modular.detach({ -1, 1 });
 
-	modular.attach({ -2, 1 }, turret);
+	REQUIRE(mark::error::code::success == modular.attach({ -2, 1 }, turret));
 	turret = modular.detach({ -2, 1 });
 
-	modular.attach({ 0, 1 }, turret);
+	REQUIRE(mark::error::code::success == modular.attach({ 0, 1 }, turret));
 	turret = modular.detach({ 0, 1 });
 
 	// Left
-	modular.attach({ -1, -3 }, turret);
+	REQUIRE(mark::error::code::success == modular.attach({ -1, -3 }, turret));
 	turret = modular.detach({ -1, -3 });
 
-	modular.attach({ -2, -3 }, turret);
+	REQUIRE(mark::error::code::success == modular.attach({ -2, -3 }, turret));
 	turret = modular.detach({ -2, -3 });
 
-	modular.attach({ 0, -3 }, turret);
+	REQUIRE(mark::error::code::success == modular.attach({ 0, -3 }, turret));
 	turret = modular.detach({ 0, -3 });
 }
 
@@ -126,7 +126,7 @@ TEST_CASE("Failed detached attach should not set internal map") {
 	mark::unit::modular modular(world, { 0, 0 }, 0);
 	std::unique_ptr<mark::module::base> core =
 		std::make_unique<mark::module::core>(rm);
-	modular.attach({ -1, -1 }, core);
+	REQUIRE(mark::error::code::success == modular.attach({ -1, -1 }, core));
 
 	mark::module::turret::info info;
 	info.resource_manager = &rm;
@@ -135,7 +135,7 @@ TEST_CASE("Failed detached attach should not set internal map") {
 	const auto& turret_ref = *turret;
 
 	REQUIRE(modular.at({ 3, -1 }) == nullptr);
-	modular.attach({ 3, -1 }, turret);
+	REQUIRE(mark::error::code::success != modular.attach({ 3, -1 }, turret));
 	turret = modular.detach({ 3, -1 });
 	REQUIRE(modular.at({ 3, -1 }) == nullptr);
 }
@@ -146,14 +146,14 @@ TEST_CASE("Remove dead modules") {
 	mark::unit::modular modular(world, { 0, 0 }, 0);
 	std::unique_ptr<mark::module::base> core =
 		std::make_unique<mark::module::core>(rm);
-	modular.attach({ -1, -1 }, core);
+	REQUIRE(mark::error::code::success == modular.attach({ -1, -1 }, core));
 
 	mark::module::turret::info info;
 	info.cur_health = 0.f;
 	info.resource_manager = &rm;
 	std::unique_ptr<mark::module::base> turret =
 		std::make_unique<mark::module::turret>(info);
-	modular.attach({ 1, -1 }, turret);
+	REQUIRE(mark::error::code::success == modular.attach({ 1, -1 }, turret));
 	mark::tick_context context(rm);
 	context.dt = 0.15;
 	modular.tick(context);
