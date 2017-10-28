5d6380fcf62703d322663e84fa5f1426035bcada
Lukasz Wrona <lukasz.andrzej.wrona@gmail.com>
2017-10-28 16:09:52 +0100

    REFACTOR: Make turret_base constructors protected.

--- game/module_base_turret.h
+++ game/module_base_turret.h
@@ -11,15 +11,16 @@ class base;
 namespace module {
 class base_turret : public module::base {
 public:
-	base_turret(
-		vector<unsigned> size,
-		const std::shared_ptr<const resource::image>& image);
-	base_turret(resource::manager&, const YAML::Node& node);
 	void target(vector<double> pos) override;
 	void shoot(vector<double> pos, bool release) override;
 	void queue(vector<double> pos, bool release) override;
 	auto passive() const noexcept -> bool override;
 protected:
+	base_turret(
+		vector<unsigned> size,
+		const std::shared_ptr<const resource::image>& image);
+	base_turret(resource::manager&, const YAML::Node& node);
+
 	void tick_ai();
 	// Returns true if there are objects still to be destroyed before
 	// switching to manual control
