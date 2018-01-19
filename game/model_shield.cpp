9e47b86ce742a8be7c93bed448930a81383f7b41
Lukasz Wrona <lukasz.andrzej.wrona@gmail.com>
2018-01-19 00:27:35 +0000

    REFACTOR: model_shield

--- game/model_shield.cpp
+++ game/model_shield.cpp
@@ -4,36 +4,35 @@
 #include "tick_context.h"
 #include "model_shield.h"
 
-mark::model::shield::shield(
-	resource::manager& rm,
-	float radius):
-	m_adsr(0, 255, 0.1f, 1),
-	m_lfo(1.f, rm.random(0.f, 1.f)),
-	m_image_reflection(rm.image("shield-reaction.png")),
-	m_image_shield(rm.image("shield.png")),
-	m_radius(radius) { }
+mark::model::shield::shield(resource::manager& rm, float radius)
+	: m_adsr(0, 255, 0.1f, 1)
+	, m_lfo(1.f, rm.random(0.f, 1.f))
+	, m_image_reflection(rm.image("shield-reaction.png"))
+	, m_image_shield(rm.image("shield.png"))
+	, m_radius(radius) { }
 
-void mark::model::shield::tick(
-	tick_context & context,
-	vector<double> pos) {
-
+void mark::model::shield::tick(tick_context& context, vector<double> pos) {
 	m_lfo.tick(context.dt);
 	m_adsr.tick(context.dt);
-	const auto shield_opacity = static_cast<uint8_t>((m_lfo.get() * 0.5f + 0.5f) * 255.f);
-	const auto reflection_opacity = static_cast<uint8_t>(m_adsr.get());
-	const auto rotation = static_cast<float>(atan(m_trigger_pos - pos));
-
-	sprite args;
-	args.image = m_image_shield;
-	args.pos = pos;
-	args.size = m_radius;
-	args.color = sf::Color(150, 255, 255, shield_opacity);
-	context.sprites[1].emplace_back(args);
+	context.sprites[1].emplace_back([&] {
+		const auto shield_opacity = static_cast<uint8_t>((m_lfo.get() * 0.5f + 0.5f) * 255.f);
+		sprite args;
+		args.image = m_image_shield;
+		args.pos = pos;
+		args.size = m_radius;
+		args.color = sf::Color(150, 255, 255, shield_opacity);
+		return args;
+	}());
 	
-	args.image = m_image_reflection;
-	args.rotation = rotation;
-	args.color = sf::Color(150, 255, 255, reflection_opacity);
-	context.sprites[1].emplace_back(args);
+	context.sprites[1].emplace_back([&] {
+		sprite args;
+		args.image = m_image_shield;
+		args.image = m_image_reflection;
+		args.rotation = static_cast<float>(atan(m_trigger_pos - pos));
+		const auto reflection_opacity = static_cast<uint8_t>(m_adsr.get());
+		args.color = sf::Color(150, 255, 255, reflection_opacity);
+		return args;
+	}());
 }
 
 void mark::model::shield::trigger(vector<double> pos) {
