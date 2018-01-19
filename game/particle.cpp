6d97ceebc7c935b645ad6873f2903c264edd6fb6
Lukasz Wrona <lukasz.andrzej.wrona@gmail.com>
2018-01-19 00:22:41 +0000

    REFACTOR: Prettify particle.

--- game/particle.cpp
+++ game/particle.cpp
@@ -17,36 +17,45 @@ static auto validate(const mark::particle::info& attr) {
 mark::particle::particle(const particle::info& info):
 	particle::particle(::validate(info), true) { }
 
-mark::particle::particle(const particle::info& attr, bool) :
-	m_image(std::move(attr.image)),
-	m_pos(attr.pos),
-	m_lifespan(attr.lifespan),
-	m_cur_lifespan(attr.lifespan),
-	m_velocity(attr.velocity),
-	m_direction(attr.direction),
-	m_size(attr.size),
-	m_color(attr.color),
-	m_layer(attr.layer) { }
+mark::particle::particle(const particle::info& attr, bool)
+	: m_image(move(attr.image))
+	, m_pos(attr.pos)
+	, m_lifespan(attr.lifespan)
+	, m_cur_lifespan(attr.lifespan)
+	, m_velocity(attr.velocity)
+	, m_direction(attr.direction)
+	, m_size(attr.size)
+	, m_color(attr.color)
+	, m_layer(attr.layer) { }
 
-void mark::particle::tick(double dt, std::map<int, std::vector<std::variant<sprite, path, rectangle>>>& sprites) {
-	const auto direction = rotate(vector<float>(1.f, 0.f), m_direction);
-	m_pos += vector<double>(direction) * dt * static_cast<double>(m_velocity);
-	const auto image_size = m_image->size();
-	const auto frame_count = image_size.x / image_size.y;
-	const auto frame = static_cast<unsigned>((1.f - m_cur_lifespan / m_lifespan) * static_cast<float>(frame_count));
-	sf::Color color;
-	if (frame_count == 1) {
-		color = sf::Color(m_color.r, m_color.g, m_color.b, static_cast<uint8_t>(255.f * (m_cur_lifespan / m_lifespan)));
-	} else {
-		color = sf::Color::White;
-	}
-	sprite info;
-	info.image = m_image;
-	info.pos = m_pos;
-	info.size = m_size;
-	info.rotation = m_direction;
-	info.frame = frame;
-	info.color = color;
-	sprites[m_layer].emplace_back(info);
+void mark::particle::tick(
+	double dt,
+	std::map<int, std::vector<std::variant<sprite, path, rectangle>>>& sprites)
+{
+	sprites[m_layer].emplace_back([&] {
+		const auto frame_count = m_image->size().x / m_image->size().y;
+		const auto progress = 1.f - m_cur_lifespan / m_lifespan;
+		const auto frame = static_cast<unsigned>(progress * static_cast<float>(frame_count));
+		const auto color =
+			frame_count == 1
+			? sf::Color(
+				m_color.r,
+				m_color.g,
+				m_color.b,
+				static_cast<uint8_t>(255.f * (m_cur_lifespan / m_lifespan)))
+			: sf::Color::White;
+		sprite info;
+		info.image = m_image;
+		info.pos = m_pos;
+		info.size = m_size;
+		info.rotation = m_direction;
+		info.frame = frame;
+		info.color = color;
+		return info;
+	}());
+	m_pos += [&] {
+		const auto direction = rotate(vector<float>(1.f, 0.f), m_direction);
+		return vector<double>(direction) * dt * static_cast<double>(m_velocity);
+	}();
 	m_cur_lifespan -= static_cast<float>(dt);
 }
