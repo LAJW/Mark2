c0638b7fe72d5ea99f34df061049fe405f6c0eca
Lukasz Wrona <lukasz.andrzej.wrona@gmail.com>
2018-01-19 00:41:40 +0000

    REFACTOR: Prettify vector.cpp

--- game/vector.cpp
+++ game/vector.cpp
@@ -6,18 +6,19 @@ auto mark::distance(const float alpha, const vector<double> point) noexcept -> d
 	return std::abs(a * point.x + point.y) / std::sqrt(a * a + 1);
 }
 
-auto mark::get_line(const vector<double> start, const vector<double> end) noexcept -> vector<double> {
+auto mark::get_line(
+	const vector<double> start, const vector<double> end) noexcept
+	-> vector<double>
+{
 	if (start.x == end.x) {
 		if (start.y == end.y) {
 			return { 0, start.y };
-		} else {
-			return { start.x, NAN };
 		}
-	} else {
-		const auto a = (start.y - end.y) / (start.x - end.x);
-		const auto b = start.y - a * start.x;
-		return { a, b };
+		return { start.x, NAN };
 	}
+	const auto a = (start.y - end.y) / (start.x - end.x);
+	const auto b = start.y - a * start.x;
+	return { a, b };
 }
 
 auto mark::intersect(
@@ -95,45 +96,51 @@ auto mark::intersect(
 		// everything but vertical line
 		const auto c = line.x;
 		const auto d = line.y;
-		const auto a = center.x;
-		const auto b = center.y;
-		const auto r = static_cast<double>(radius);
-		const auto e = d - b;
-		const auto A = c * c + 1;
-		const auto B = 2.0 * c * e - 2.0 * a;
-		const auto C = a * a + e * e - r * r;
-		const auto delta2 = B * B - 4 * A * C;
-		if (delta2 >= 0) {
+		const auto [A, B, delta2] = [&] {
+			const auto a = center.x;
+			const auto b = center.y;
+			const auto r = static_cast<double>(radius);
+			const auto e = d - b;
+			const auto A = c * c + 1;
+			const auto B = 2.0 * c * e - 2.0 * a;
+			const auto C = a * a + e * e - r * r;
+			const auto delta2 = B * B - 4 * A * C;
+			return std::make_tuple(A, B, delta2);
+		}();
+		if (delta2 <= 0)
+			return { };
+		const auto[p1, p2] = [&] {
 			const auto delta = std::sqrt(delta2);
 			const auto x1 = (-B + delta) / (2.0 * A);
 			const auto x2 = (-B - delta) / (2.0 * A);
 			const auto y1 = c * x1 + d;
 			const auto y2 = c * x2 + d;
-			const auto p1 = vector<double>(x1, y1);
-			const auto p2 = vector<double>(x2, y2);
+			return std::make_pair(vector<double>(x1, y1), vector<double>(x2, y2));
+		}();
+		const bool p1_in_range = p1.x >= lx && p1.x <= ux
+			&& p1.y >= ly && p1.y <= uy;
+		const bool p2_in_range = p2.x >= lx && p2.x <= ux
+			&& p2.y >= ly && p2.y <= uy;
+		const auto p1_closer = [&] {
 			const auto length1 = length(p1 - segment.first);
 			const auto length2 = length(p2 - segment.first);
-
-			bool p1_in_range = p1.x >= lx && p1.x <= ux
-				&& p1.y >= ly && p1.y <= uy;
-			bool p2_in_range = p2.x >= lx && p2.x <= ux
-				&& p2.y >= ly && p2.y <= uy;
-			bool p1_closer = length1 < length2;
-			if (p1_in_range && (p2_in_range && p1_closer || !p2_in_range)) {
-				return p1;
-			} else if (p2_in_range) {
-				return p2;
-			}
+			return length1 < length2;
+		}();
+		if (p1_in_range && (p2_in_range && p1_closer || !p2_in_range)) {
+			return p1;
+		} else if (p2_in_range) {
+			return p2;
 		}
 	} else if (!std::isnan(line.x) && std::abs(line.x - center.x) <= radius) {
 		// vertical line
-		const auto a = line.x;
-		const auto b = line.y;
-		const auto r = static_cast<double>(radius);
-		const auto A = center.x - line.x;
-		const auto B = std::sqrt(A * A + r * r);
-		const auto y1 = center.y - B;
-		const auto y2 = center.y + B;
+		const auto [y1, y2] = [&] {
+			const auto r = static_cast<double>(radius);
+			const auto A = center.x - line.x;
+			const auto B = std::sqrt(A * A + r * r);
+			const auto y1 = center.y - B;
+			const auto y2 = center.y + B;
+			return std::make_pair(y1, y2);
+		}();
 		const auto length1 = std::abs(y1 - segment.first.y);
 		const auto length2 = std::abs(y2 - segment.first.y);
 		const auto y1_in_range = y1 >= ly && y1 <= uy;
@@ -149,12 +156,16 @@ auto mark::intersect(
 }
 
 // Calculate new rotation for an entity based on angular velocity, lookat direction, etc.
-auto mark::turn(vector<double> new_direction, float current_rotation, float angular_velocity, double dt) -> float {
-	const auto turn_direction = sgn(atan(rotate(new_direction, -current_rotation)));
-	const auto rot_step = static_cast<float>(turn_direction  * angular_velocity * dt);
+auto mark::turn(
+	vector<double> new_direction,
+	float current_rotation,
+	float angular_velocity,
+	double dt) -> float
+{
 	if (std::abs(atan(rotate(new_direction, -current_rotation))) < angular_velocity * dt) {
 		return static_cast<float>(atan(new_direction));
-	} else {
-		return current_rotation + rot_step;
 	}
+	const auto turn_direction = sgn(atan(rotate(new_direction, -current_rotation)));
+	const auto rot_step = static_cast<float>(turn_direction  * angular_velocity * dt);
+	return current_rotation + rot_step;
 }
