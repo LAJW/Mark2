3f8982d2935be5fd2fd114308005f839c6eaff81
Lukasz A.J. Wrona <lukasz.andrzej.wrona@gmail.com>
2018-03-18 04:53:04 +0000

    FEAT: Inheriting property values in templates.

--- game/property_manager.cpp
+++ game/property_manager.cpp
@@ -9,8 +9,15 @@ mark::property_manager::property_manager(resource::manager& rm)
 
 void mark::property_manager::deserialise(const YAML::Node& node)
 {
+	std::vector<std::function<void()>> hooks;
 	for (const auto [key, config] : m_properties) {
-		config.deserialise(config.value_ref, node[key], m_rm);
+		let hook = config.deserialise(config.value_ref, node[key], *this, m_rm);
+		if (hook) {
+			hooks.push_back(*hook);
+		}
+	}
+	for (let& hook : hooks) {
+		hook();
 	}
 }
 
