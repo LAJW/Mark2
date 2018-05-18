ccb1503c32813cdb9d4a807a83092e5ebfef6c16
Lukasz A.J. Wrona <lukasz.andrzej.wrona@gmail.com>
2018-05-18 01:07:35 +0100

    FEAT: interface::can_detach

--- game/interface/container.h
+++ game/interface/container.h
@@ -15,6 +15,7 @@ public:
 	// Position at which the item is attached (the top left corner of an item)
 	virtual auto pos_at(vi32 pos) const noexcept -> std::optional<vi32> = 0;
 	virtual auto detach(vi32 pos) -> interface::item_ptr = 0;
+	virtual auto can_detach(vi32 pos) const noexcept -> bool = 0;
 
 protected:
 	~container() = default;
