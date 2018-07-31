5f5d0f5b486b3b0f7bfc8fcadce177d84b01fd1c
Lukasz A.J. Wrona <lukasz.andrzej.wrona@gmail.com>
2018-07-31 09:31:28 +0100

    TEST: Use BDD-Style for core tests.

--- test/module/core.cpp
+++ test/module/core.cpp
@@ -1,26 +1,29 @@
-#include <catch.hpp>
 #include "../core_env.h"
+#include <catch.hpp>
 
-TEST_CASE("Core module pos")
+SCENARIO("core")
 {
-	using namespace mark;
-	core_env core_env;
-	let& core = core_env.core;
-	REQUIRE(core.pos() == vd(0, 0));
-	core_env.modular->pos(vd(13., 13.));
-	REQUIRE(core.pos() == vd(13, 13));
-}
-
-TEST_CASE("Core module should not be detachable")
-{
-	using namespace mark;
-	core_env core_env;
-	REQUIRE(core_env.core.detachable() == false);
-}
-
-TEST_CASE("Core module should be passive")
-{
-	using namespace mark;
-	core_env core_env;
-	REQUIRE(core_env.core.passive() == true);
+	GIVEN("undamaged core")
+	{
+		using namespace mark;
+		core_env core_env;
+		WHEN("we do nothing")
+		{
+			THEN("Core module position should be the same as the ship position")
+			{
+				let& core = core_env.core;
+				REQUIRE(core.pos() == vd(0, 0));
+				core_env.modular->pos(vd(13., 13.));
+				REQUIRE(core.pos() == vd(13, 13));
+			}
+			THEN("Core module should not be detachable")
+			{
+				REQUIRE(core_env.core.detachable() == false);
+			}
+			THEN("Core module should be passive")
+			{
+				REQUIRE(core_env.core.passive() == true);
+			}
+		}
+	}
 }
