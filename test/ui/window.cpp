49730c0c7e703281b5db51b735a980d02b956d2c
Lukasz A.J. Wrona <lukasz.andrzej.wrona@gmail.com>
2018-08-13 07:13:38 +0100

    TEST: Add test for the const version of window::children.

--- test/ui/window.cpp
+++ test/ui/window.cpp
@@ -37,5 +37,17 @@ SCENARIO("Window")
 				REQUIRE(&result.front().get() == &child_window_ref);
 			}
 		}
+		WHEN("We call immutable children on it")
+		{
+			let result = static_cast<const ui::window&>(window).children();
+			THEN("It should return a vector with one element in it")
+			{
+				REQUIRE(result.size() == 1);
+			}
+			THEN("The only element in the vector should be the child window")
+			{
+				REQUIRE(&result.front().get() == &child_window_ref);
+			}
+		}
 	}
 }
