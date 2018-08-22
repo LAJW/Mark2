88ad2208fc17df6e0e00657b87c055486fa33312
Lukasz A.J. Wrona <lukasz.andrzej.wrona@gmail.com>
2018-08-22 18:53:12 +0100

    FIX: find_if algorithm

--- game/algorithm/find_if.h
+++ game/algorithm/find_if.h
@@ -3,8 +3,8 @@
 
 namespace mark {
 template <typename container_t, typename pred_t>
-auto find_if(container_t& container, pred_t pred)
+auto find_if(const container_t& container, pred_t pred)
 {
-	return std::find(std::begin(container), std::end(container), pred);
+	return std::find_if(std::begin(container), std::end(container), pred);
 }
 } // namespace mark
