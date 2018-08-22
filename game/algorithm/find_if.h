e139881b58b742938ca79266ace6705e690c0b86
Lukasz A.J. Wrona <lukasz.andrzej.wrona@gmail.com>
2018-08-22 22:42:23 +0100

    FIX: Disallow rvalues from being inserted into find_if.

--- game/algorithm/find_if.h
+++ game/algorithm/find_if.h
@@ -3,8 +3,13 @@
 
 namespace mark {
 template <typename container_t, typename pred_t>
-auto find_if(const container_t& container, pred_t pred)
+[[nodiscard]] auto find_if(container_t& container, pred_t pred)
 {
+	// NOTE TO THE FUTURE SELF: Before you have a brilliant idea of turning
+	// container_t into non-const, keep in mind that this function returns an
+	// iterator. So container has to exist longer than this function. Putting it
+	// as an argument of this makes little sense, as the iterator will be
+	// invalidated, because the vector will no longer be there. You're welcome.
 	return std::find_if(std::begin(container), std::end(container), pred);
 }
 } // namespace mark
